32569afdad52b55db36bfdc055cf5e46d32a3acf
Buse Karabıyık <karabiyikbuse04@gmail.com>
2023-08-02 12:08:09 +0300

    Initial Commit

---
+++ FlagQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlagQuiz
+//
+//  Created by Buse Karabıyık on 2.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
